55f714b77f032daef635734c502cbe414c1bf442
Shahi77 <shahiinfinity02@gmail.com>
2024-07-05 13:36:49 +0530

    string,js

--- string.js
+++ string.js
@@ -2,6 +2,7 @@ const client = require("./client");
 
 async function init() {
   await client.set("msg:1", "Hey from node.js");
+  //to set expiry: await client.expire("msg:1",10)
   const res = await client.get("msg:1");
   console.log("Result: ", res);
 }
